a26d257881175fb9f5968ab9f2fd27b4a7ef6d93
pavithra durairaj <pavithra61192@gmail.com>
2022-08-11 12:36:59 -0600

    first commit

---
+++ src/main/java/org/comit/course/Comit01CalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comit01CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comit01CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/comit/course/Comit01CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comit01CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
